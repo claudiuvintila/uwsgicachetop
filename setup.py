c6fe82774c4d3f969fe1e3d9ab18e15af46a1bfe
claudiuvintila <claudiu.vintila@gmail.com>
2021-07-14 16:48:22 +0300

    support for python 3.7

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name='uwsgicachetop',
-    version='1.0.0',
-    url='https://github.com/goir/uwsgicachetop',
-    download_url = 'https://github.com/goir/uwsgicachetop/archive/1.0.0.tar.gz',
+    name='python3-uwsgicachetop',
+    version='1.1.0',
+    url='https://github.com/claudiuvinttila/uwsgicachetop',
+    download_url = 'https://github.com/claudiuvinttila/uwsgicachetop/archive/1.1.0.tar.gz',
     license='GPLv2',
     author='Goir',
     author_email='goir@goirware.de',
